8b4a02c3bd5918cb70553e944b009b31b245f38e
Connor Rohlfing Peters <connor.rohl.peters@gmail.com>
2026-04-09 11:12:52 -0400

    Update jest.config.mjs

--- packages/angular/jest.config.mjs
+++ packages/angular/jest.config.mjs
@@ -2,6 +2,9 @@ export default {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.mjs"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  moduleNameMapper: {
+    "^@open-passkey/sdk$": "<rootDir>/node_modules/@open-passkey/sdk/dist/index.js",
+  },
   transform: {
     "^.+\\.(ts|mjs|js|html)$": [
       "jest-preset-angular",
